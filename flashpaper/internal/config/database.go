8a5f8c8e2f0d756c6130b5db689e6fe45c091bf2
Direwen <direwensik@gmail.com>
2025-12-06 16:22:52 +0630

    chore: configure backend to connect to cloud db

--- flashpaper/internal/config/database.go
+++ flashpaper/internal/config/database.go
@@ -17,15 +17,20 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	var err error
+	var dsn string
 
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	port := os.Getenv("DB_PORT")
-	sslmode := os.Getenv("DB_SSLMODE")
+	if os.Getenv("DB_URL") != "" {
+		dsn = os.Getenv("DB_URL")
+	} else {
+		host := os.Getenv("DB_HOST")
+		user := os.Getenv("DB_USER")
+		password := os.Getenv("DB_PASSWORD")
+		dbname := os.Getenv("DB_NAME")
+		port := os.Getenv("DB_PORT")
+		sslmode := os.Getenv("DB_SSLMODE")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", host, user, password, dbname, port, sslmode)
+		dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", host, user, password, dbname, port, sslmode)
+	}
 
 	//Connect to DB
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
